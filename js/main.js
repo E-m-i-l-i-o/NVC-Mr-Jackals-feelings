8b9d86ec8e081a1e51f3deb236584ffcdacadcec
E-m-i-l-i-o <132336502+E-m-i-l-i-o@users.noreply.github.com>
2023-05-25 13:14:46 +0200

    obstacles three leaf and speed

--- js/main.js
+++ js/main.js
@@ -92,7 +92,7 @@ class Obstacles {
     
     constructor (){
         this.width = 9;
-        this.height = 3; 
+        this.height = 5; 
         this.positionX = Math.floor(Math.random()* 100 - this.width);
         this.positionY = 100;
 
@@ -133,7 +133,7 @@ const obstaclesArr = []; //outside so that it can be accessed from several sibli
 setInterval(() => {
     const newObstacle = new Obstacles();
     obstaclesArr.push(newObstacle);
-}, 4000);
+}, 6000);
 
 // move those obstacles
 setInterval(()=>{
@@ -176,8 +176,8 @@ setInterval(()=>{
 class ObstaclesTwo {
     
     constructor (){
-        this.width = 5 ;
-        this.height = 8; 
+        this.width = 7 ;
+        this.height = 10; 
         this.positionX = Math.floor(Math.random()* 100 - this.width);
         this.positionY = 100;
 
@@ -247,8 +247,95 @@ setInterval(()=>{
 
     });
 
-},200);
+},100);
 
+
+
+
+
+
+
+
+// *********************
+// *********************
+// *** obstacles 3  *****
+// *********************
+// *********************
+class ObstaclesThree {
+    
+    constructor (){
+        this.width = 3 ;
+        this.height = 22; 
+        this.positionX = Math.floor(Math.random()* 100 - this.width);
+        this.positionY = 100;
+
+
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement() {
+         // step1: create the element
+         this.domElement = document.createElement('div');
+
+         // step2: add content or modify (ex. innerHTML...)
+         this.domElement.className = 'obstacleThree';
+         this.domElement.style.width = this.width + 'vw';
+         this.domElement.style.height = this.height + "vh";
+         this.domElement.style.left = this.positionX +"vw";
+         this.domElement.style.bottom = this.positionY + 'vh';
+         //this.domElement.style.backgroundColor = '#96070c'
+         this.domElement.style.imageRendering
+         this.domElement.style.position = 'absolute'
+ 
+         //step3: append to the dom: `parentElm.appendChild()`
+        const parentElm = document.getElementById('board') 
+         parentElm.appendChild(this.domElement);
+     }
+     moveDown(){
+         this.positionY--;
+         this.domElement.style.bottom = this.positionY + "vh";
+
+    }
+
+}
+
+//create new obstacles
+const obstaclesThreeArr = []; //outside so that it can be accessed from several siblings
+
+setInterval(() => {
+    const newObstacle = new ObstaclesThree();
+    obstaclesThreeArr.push(newObstacle);
+}, 7000);
+
+// move those obstacles
+setInterval(()=>{
+    obstaclesThreeArr.forEach((obstacleItem)=> {
+        obstacleItem.moveDown();
+
+        if (obstacleItem.positionX < player.positionX + player.width &&
+            obstacleItem.positionX + obstacleItem.width > player.positionX &&  //we move a new element and we check if there is a collision all wihtin the same loop
+            obstacleItem.positionY < player.positionY + player.height &&
+            obstacleItem.height + obstacleItem.positionY > player.positionY) {
+            console.log("game over my fren");
+            //gameOver()
+            location.href= 'startpage.html' //  send player to start again
+            }
+            //detect if obstacle needs to be removed once out of sight from the viewport. for that obstacle first needs to reach  the Y axis = 0 adn then we substract teh heigth of the obstacle, so taht it dissapears once we dont see it
+            if (obstacleItem.positionY < 0 - obstacleItem.height) {
+                //1. remove from the array of obstacles. we can use .shift in order to delete teh 1st element of our array (which is the first one reaching teh bottom of the viewport)
+                    obstaclesThreeArr.shift();
+
+
+                //2. remove the obstacle elm from the dom with the .remove() method that we can use to this DOM element
+                //teh obsctacle  we want to delete is in the instance 'obstacleItem' of the class 'Obstacle'. thats how we can accesss positionY, for example.
+                //the DOM element of that obstacle is in the 'domElement'. now we can apply the remove() method to that.
+                obstacleItem.domElement.remove()
+}                          
+
+    });
+
+},400);
 
 
 
